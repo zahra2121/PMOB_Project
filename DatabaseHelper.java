3a44474c20a1f9f5a630af13ad15d1f6d4417542
zahra2121 <87288056+zahra2121@users.noreply.github.com>
2022-12-25 08:28:57 +0700

    Add files via upload

---
+++ DatabaseHelper.java
@@ -0,0 +1,58 @@
+package com.hikmatuzzahra.pmob.project12;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper{
+    public static final String DATABASE_NAME ="register.db";
+    public static final String TABLE_NAME ="registeruser";
+    public static final String COL_1 ="ID";
+    public static final String COL_2 ="username";
+    public static final String COL_3 ="password";
+    public static final String COL_4 ="name";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("CREATE TABLE registeruser (ID INTEGER PRIMARY  KEY AUTOINCREMENT, username TEXT, name TEXT, password TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL(" DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+
+    public long addUser(String user, String name, String password){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("username",user);
+        contentValues.put("name",name);
+        contentValues.put("password",password);
+        long res = db.insert("registeruser",null,contentValues);
+        db.close();
+        return  res;
+    }
+
+    public boolean checkUser(String username,String name, String password){
+        String[] columns = { COL_1 };
+        SQLiteDatabase db = getReadableDatabase();
+        String selection = COL_2 + "=?" + " and " + COL_3 + "=?" + " and " + COL_4 + "=?";
+        String[] selectionArgs = { username, password, name };
+        Cursor cursor = db.query(TABLE_NAME,columns,selection,selectionArgs,null,null,null);
+        int count = cursor.getCount();
+        cursor.close();
+        db.close();
+
+        if(count>0)
+            return  true;
+        else
+            return  false;
+    }
+}
